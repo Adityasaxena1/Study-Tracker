5eafdd26a62ed432176c75fd89aa46316617db10
Adityasaxena1 <144054604+Adityasaxena1@users.noreply.github.com>
2023-09-07 22:16:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime
+
+pixela_endpoints = "https://pixe.la/v1/users"
+
+TOKEN = "hviseuhirhe38y3ihi3hk"
+USERNAME = "adityasaxena"
+GRAPH_ID = "graph1"
+
+user_parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+
+}
+
+# response = requests.post(url=pixela_endpoints, json=user_parameters)
+# print(response.text)
+
+
+graph_endpoint = f"{pixela_endpoints}/{USERNAME}/graphs"
+
+graph_parameters = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.get(url=graph_endpoint, params=graph_parameters, headers=headers)
+# print(response.text)
+
+today = datetime.now()
+
+post_pixela_endpoints = f"{pixela_endpoints}/{USERNAME}/graphs/{GRAPH_ID}"
+
+post_parameters = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hours you have studied today?")
+}
+
+response = requests.post(url=post_pixela_endpoints, json=post_parameters, headers=headers)
+print(response.text)
+
+url =f"{pixela_endpoints}/{USERNAME}/graphs/{GRAPH_ID}/20230720"
+put_endpoints={
+    "quantity":"10.0"
+}
+
+# response = requests.delete(url=url, headers=headers)
+# print(response.text)
+
+
